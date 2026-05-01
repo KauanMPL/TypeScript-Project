c60dea7007b20938ac78557418c78a221c5e3493
KauanMPL <kauanpavaneli@gmail.com>
2026-04-30 21:06:12 -0300

    implementa fluxo de dados entre componentes com props tipadas

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,21 @@ import "./App.css";
 import CardList from "./Components/CardList/CardList";
 import Card from './Components/Card/Card';
 import Search from "./Components/Search/Search";
+import { ChangeEvent, SyntheticEvent, useState } from "react";
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(e);
+  };
+  const onClick = (e: SyntheticEvent) => {
+    console.log(e);
+  };
   return (
     <div className="App">
-      <Search />
+      <Search onClick={onClick} search={search} handleChange={handleChange} />
       <CardList />
     </div>
   );
--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,23 +1,18 @@
-import React, { ChangeEvent,SyntheticEvent,useState } from 'react'
+import React, { ChangeEvent,SyntheticEvent, useState } from 'react'
 
-type Props = {}
+interface Props  {
+    onClick(e: SyntheticEvent): void;
+    search: string | undefined;
+    handleChange(e: ChangeEvent<HTMLInputElement>): void;
+}
 
-const Search: React.FC<Props> = () => {
-  const [search, setSearch] = useState<string>("");
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-    console.log(e);
-  };
-  const onclick = (e: SyntheticEvent) =>{
-    console.log(e);
-  };
+const Search: React.FC<Props> = ({onClick, search, handleChange}:Props) => {
 
   return (
     <div>  
       <input value={search} onChange={(e) => handleChange(e)} ></input>
-      <button onClick={(e) =>  onclick(e)}/>
+      <button onClick={(e) =>  onClick(e)}/>
     </div>
   )
 }
-export default Search
+export default Search;
