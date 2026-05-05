fc3937f3ed1ecac47d216de4a6228e23acc2e3b3
KauanMPL <kauanpavaneli@gmail.com>
2026-05-05 12:06:16 -0300

    implementar hook useEffect e busca de dados da API

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,19 +1,28 @@
 import axios from "axios";
-import { CompanySearch } from "./company";
-
-interface SearchResponse {
-    data: CompanySearch[];
-}
+import { CompanySearch, CompanyProfile } from "./company";
 
 export const searchCompanies = async (query: string) => {
-    try {
-        const response = await axios.get<SearchResponse>(
-            `https://financialmodelingprep.com/stable/search-symbol?query=${query}&apikey=${process.env.REACT_APP_API_KEY}`
-        );
+  try {
+    if (!query.trim()) return [];
 
-        return response.data;
-    } catch (error: any) {
-        console.log("Erro:", error?.message || error);
-        return [];
-    }
+    const response = await axios.get<CompanySearch[]>(
+      `https://financialmodelingprep.com/stable/search-symbol?query=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+
+    return response.data;
+  } catch (error: any) {
+    console.log("Erro:", error?.message || error);
+    return [];
+  }
+};
+
+export const getCompanyProfile = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyProfile[]>(
+      `https://financialmodelingprep.com/stable/profile?symbol=${query}&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message from API:", error.message);
+  }
 };
