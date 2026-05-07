5351f0535f18a59b8c40fbc4c4ee47b06dcef0c0
KauanMPL <kauanpavaneli@gmail.com>
2026-05-07 11:45:33 -0300

    adiciona página de demonstração de resultados com tabela de dados financeiros

--- frontend/src/api.tsx
+++ frontend/src/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { CompanySearch, CompanyProfile, CompanyKeyMetrics } from "./company";
+import { CompanySearch, CompanyProfile, CompanyKeyMetrics, CompanyIncomeStatement } from "./company";
 
 export const searchCompanies = async (query: string) => {
   try {
@@ -31,6 +31,17 @@ export const getKeyMetrics = async (query: string) => {
   try {
     const data = await axios.get<CompanyKeyMetrics[]>(
       `https://financialmodelingprep.com/api/v3/key-metrics-ttm/${query}?limit=40&apikey=${process.env.REACT_APP_API_KEY}`
+    );
+    return data;
+  } catch (error: any) {
+    console.log("error message: ", error.message);
+  }
+};
+
+export const getIncomeStatement = async (query: string) => {
+  try {
+    const data = await axios.get<CompanyIncomeStatement[]>(
+      `https://financialmodelingprep.com/api/v3/income-statement/${query}?limit=50&apikey=${process.env.REACT_APP_API_KEY}`
     );
     return data;
   } catch (error: any) {
