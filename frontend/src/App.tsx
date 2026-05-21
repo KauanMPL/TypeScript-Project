b233378228ee8c8c563a07a5d7dfaf6887c21b9f
KauanMPL <kauanpavaneli@gmail.com>
2026-05-21 11:20:34 -0300

    Cria AuthService e tratamento de erros com react-toastify
    Adiciona os serviços de login e registro no front-end utilizando Axios. Implementa também uma estrutura helper para interceptar erros da API e exibi-los através de toasts.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router";
 import "./App.css";
 import Navbar from "./Components/NavBar/NavBar";
-
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return <>
     <Navbar />
